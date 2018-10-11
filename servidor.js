828f4dd2fee9f79243ebf34133e5fa14303c68ea
CristoGuillermoAlvarezSilva <35870620+CristoGuillermoAlvarezSilva@users.noreply.github.com>
2018-10-11 09:50:27 -0500

    commit

--- servidor.js
+++ servidor.js
@@ -1,30 +1,55 @@
+//npm init -y
 //npm install express
 //npm install jsonwebtokens
-//npm install -y
 
 var express = requiere('express');
 var jwt = requiere('jsonwebtoken');
 var puerto = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.json());
+app.use(express.json());//midleware
 
 app.get('/calificaciones',function (req,res){
-    res.json({
-        mensaje: 'Bienvenido al Api del Kardex'
+    console.log('Token recibido' + req.query.token);
+    jwt.verify(req.query.token,'clavesupersecreta',function(){
+        function(error){
+            if(error){
+                res.status(403).json({ mensaje: 'Autorizacion no valida' });
+            }
+            else{
+                res.json({
+                    mensaje: 'Aqui estan las calificaciones...'
+                });
+            }
+        }
     });
 });
 
 app.post('/login',function(req,res){
+    //simular la base de datos
+    var alumno{
+        email: 'alumno@uaslp.mx',
+        password: ´123´
+    };
+    if(req.body.email == alumno.email && req.body.password == alumno.password){
     var token = jwt.sign({
-        usuario: 'alumno'
-    },'clavesupersecreta',{expiresIn: '60s'});
+        usuario: 'alumno',
+        nombre: 'Raul',
+        claveUnica: ´123456´,
+    },'clavesupersecreta',{expiresIn: '1h'});
     console.log('Token generado: '+token);
     res.json({
         elToken: token
     });
+    }
+    else{
+        res.status(401).json({
+            mensaje: 'Credenciales no validas',
+            elToken: null
+        });
+    }
 });
-
+//https://www.npmjs.com/package/jsonwebtoken pagina con complmentos de token
 //jwt.io pagina para ver el contenido del token
 
 app.listen(puerto,function(){
